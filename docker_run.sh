92b2d8a1eade2bb5f5e741da3c8959cfe0acf195
Rob van der Ent <robvde65@live.com>
2020-05-01 09:40:27 +0800

    initial commit

---
+++ docker_run.sh
@@ -0,0 +1,28 @@
+IP_LOOKUP="$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')"  # May not work for VPN / tun0
+IPv6_LOOKUP="$(ip -6 route get 2001:4860:4860::8888 | awk '{for(i=1;i<=NF;i++) if ($i=="src") print $(i+1)}')"  # May not work for VPN / tun0
+IP="${IP:-$IP_LOOKUP}"  # use $IP, if set, otherwise IP_LOOKUP
+#IP="10.0.10.0"
+IPv6="${IPv6:-$IPv6_LOOKUP}"  # use $IPv6, if set, otherwise IP_LOOKUP
+#IPv6="2406:..."
+DOCKER_CONFIGS="$(pwd)"  # Default of directory you run this from, update to where ever.
+
+echo "### Make sure your IPs are correct, hard code ServerIP ENV VARs if necessary\nIP: ${IP}\nIPv6: ${IPv6}"
+docker run -d \
+    --name pihole \
+    --network host \
+    -p 53:53/tcp -p 53:53/udp \
+    -p 67:67/udp \
+    -p 80:80 \
+    -p 443:443 \
+    -v "/home/pi/pihole/etc-pihole/:/etc/pihole/" \
+    -v "/home/pi/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+    -e ServerIP="${IP}" \
+    -e ServerIPv6="${IPv6}" \
+    --restart=unless-stopped \
+    --cap-add=NET_ADMIN \
+    pihole/pihole:latest
+
+echo -n "Your password for https://${IP}/admin/ is "
+docker logs pihole 2> /dev/null | grep 'password:'
+docker logs pihole 2> /dev/null | grep 'password:'
+
